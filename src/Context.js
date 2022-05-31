f73282285c580d98aea7c05f52beec22e9de1ac9
Adithya Ajith <4adhi007@gmail.com>
2022-05-31 20:15:40 +0530

    cocktail cards

--- src/Context.js
+++ src/Context.js
@@ -14,8 +14,36 @@ const AppProvider = ({ children }) => {
     try{
         const response = await fetch(`${url}${searchTerm}`)
         const data = await response.json()
-        console.log(data);
-    }catch(e){ console.log(e) }
+        const {drinks}=data
+
+        if(drinks)
+        {
+          const newCocktails = drinks.map((item) => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlcoholic,
+              strGlass,
+            } = item
+  
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcoholic,
+              glass: strGlass,
+            }
+          })
+          setCocktails(newCocktails)
+        }else{
+          setCocktails([])
+        }
+        setLoading(false)
+        
+
+    }catch(e){ console.log(e)
+    setLoading(false) }
  }
 
  useEffect(()=>{
--- src/components/Cocktaillist.js
+++ src/components/Cocktaillist.js
@@ -6,7 +6,7 @@ import {useGlobalContext} from '../Context'
 
 function Cocktaillist() {
   const {loading,cocktails}= useGlobalContext()
-
+ console.log(cocktails)
   if(loading)
  { return <Loading/>
  }
@@ -16,7 +16,14 @@ if(cocktails.length<1)
 )}
 
 return(
-  <h1>cocc</h1>
+  <section className='section'>
+  <h2 className='section-title'>Cocktails</h2>
+  <div className='cocktails-center'>
+  {cocktails.map((item)=>{
+   return <Cocktail key={item.id} {...item} />
+   } )}
+  </div>
+  </section>
 )
 
 }
